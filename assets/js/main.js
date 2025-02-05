43194e773161b95bd3ec72949b78fbf3f763c7e0
Felipe Medola Barranco <felipe.medola@hotmail.com>
2025-02-05 13:56:21 -0300

    Add função para exibir lista de Pokémons usando fetch

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,11 +5,34 @@ const offset = 0;
 const limit = 10;
 const url = `https://mwtoxml.com.br/poke.php?offset=${offset}&limit=${limit}`;
 
+function convertPokemonToLi(pokemon) {
+    return `
+    <li class="pokemon">
+        <span class="number">#001</span>
+        <span class="name">${pokemon.name}</span>
+
+        <div class="detail">
+            <ol class="types">
+                <li class="type">grass</li>
+                <li class="type">poison</li>
+            </ol>
+
+            <img class="pokeImage" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="${pokemon.name}">
+        </div> 
+    </li>
+    `
+}
+
+const pokemonList = document.getElementById('pokemonList')
+
 fetch(url)
     .then((response) => response.json())
     .then((jsonBody) => jsonBody.results)
-    .then((pokemonList) => {
-        debugger
-        console.log(pokemonList)
+    .then((pokemons) => {
+
+        for (let i = 0; i < pokemons.length; i++) {
+            const pokemon = pokemons[i];
+            pokemonList.innerHTML += convertPokemonToLi(pokemon)
+        }
     })
     .catch((error) => console.log(error))

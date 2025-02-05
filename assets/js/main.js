308f6b2d512fa64d43135545d349fc0b32385e85
Felipe Medola Barranco <felipe.medola@hotmail.com>
2025-02-05 10:26:08 -0300

    feat: adicionar fetch para obter dados da API

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,15 @@
+// PokeAPI: https://pokeapi.co/api/v2/pokemon?
+// MWApi = https://mwtoxml.com.br/poke.php?
 
 const offset = 0;
 const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+const url = `https://mwtoxml.com.br/poke.php?offset=${offset}&limit=${limit}`;
 
 fetch(url)
     .then((response) => response.json())
-    .then((jsonBody) => console.log(jsonBody))
-    .catch((error) => console.error(error))
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemonList) => {
+        debugger
+        console.log(pokemonList)
+    })
+    .catch((error) => console.log(error))
